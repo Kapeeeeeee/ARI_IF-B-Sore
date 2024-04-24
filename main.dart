82420e10fec459633787b226ed7c407d955e86e6
Kenzie Pragata <221110403@students.mikroskil.ac.id>
2024-04-24 17:26:44 +0700

    Penambahan Provider

--- main.dart
+++ main.dart
@@ -1,19 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:m05/home.dart';
+import 'package:kerkom/alamat.dart';
+import 'package:kerkom/cart.dart';
+import 'package:kerkom/diskon.dart';
+import 'package:kerkom/home.dart';
+import 'package:kerkom/payment_cont.dart';
+import 'package:kerkom/pembayaran_lanjutan.dart';
+import 'package:kerkom/metode_pembayaran.dart';
+import 'package:kerkom/provider.dart';
+import 'package:provider/provider.dart';
 
-void main(){
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "M05 Teori",
-      debugShowCheckedModeBanner: false,
-      home: Home(),
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(create: (context) => CartProvider()),
+      ChangeNotifierProvider(create: (context) => AlamatProvider()),
+      ChangeNotifierProvider(create: (context) => PromoProvider()),
+      ChangeNotifierProvider(create: (context) => PriorityProvider()),
+
+    ],
+      child: MaterialApp(
+        title: "Coba",
+        debugShowCheckedModeBanner: false,
+        home: Invoice(),
+      ),
     );
   }
 }

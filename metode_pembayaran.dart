e682e99199d3d6e97495bd0079f2edd252df6ed6
Kenzie Pragata <221110403@students.mikroskil.ac.id>
2024-04-24 17:42:57 +0700

    Page pemilihan metode pembayaran(unfinished)

---
+++ metode_pembayaran.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:kerkom/detail.dart';
+
+class Metode extends StatefulWidget {
+  const Metode({super.key});
+
+  @override
+  State<Metode> createState() => _MetodeState();
+}
+
+class _MetodeState extends State<Metode> {
+  bool isClicked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Metode Pembayaran"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              child: Text("Jenis Pembayaran"),
+              decoration: BoxDecoration(
+                color: Colors.white12,
+                border : Border(
+                  top: BorderSide(width: 1.0, color: Colors.black38), // Atas
+                  bottom: BorderSide(width: 1.0, color: Colors.black38),
+                ),
+              )
+            ),
+
+            Padding(
+              padding: EdgeInsets.all(8),
+              child: Column(
+                children: [
+                  GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        isClicked = !isClicked;
+                      });
+                    },
+                    child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white12,
+                          border : Border(
+                            top: BorderSide(color: isClicked ? Colors.black : Colors.orangeAccent,
+                            width: 1),
+                            bottom: BorderSide(color: isClicked ? Colors.black : Colors.orangeAccent,
+                                width: 1)
+                          ),
+                        ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.account_balance_wallet_rounded),
+                              Text("ARI Wallet", style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold
+                              ),),
+                              Icon(Icons.new_releases_outlined, size: 15,color: Colors.red,),
+                            ],
+                          ),
+                          Text("Balance : ${p1.uang}")
+                        ],
+                      ),
+
+                    ),
+                  ),
+                  SizedBox(height: 15,),
+                  GestureDetector(
+                    onTap: (){
+                      setState(() {
+                        isClicked = !isClicked;
+                      });
+                    },
+                    child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white12,
+                          border : Border(
+                            top: BorderSide(color: isClicked ? Colors.black : Colors.orangeAccent,
+                            width: 1),
+                            bottom: BorderSide(color: isClicked ? Colors.black : Colors.orangeAccent,
+                                width: 1)
+                          ),
+                        ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.money),
+                              Text("Cash", style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold,
+                              ),),
+                            ],
+                          ),
+                          Text("Balance : ${p1.uang}")
+                        ],
+                      ),
+
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

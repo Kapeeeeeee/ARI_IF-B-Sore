03b9d553441ddb10a049e84ba9dbb4a768815ca2
Kapeeeeeee <165145804+Kapeeeeeee@users.noreply.github.com>
2024-04-26 13:18:32 +0700

    Page comment

---
+++ commentpage.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:provider/provider.dart';
+import 'package:tugas_kelompok_semester4/project/provider.dart';
+
+
+
+class CommentPage extends StatefulWidget {
+  @override
+  _CommentPageState createState() => _CommentPageState();
+}
+
+class _CommentPageState extends State<CommentPage> {
+  int? _selectedRating;
+
+  @override
+  Widget build(BuildContext context) {
+    final commentData = Provider.of<CommentData>(context);
+    final commentsList = commentData.commentsList;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Comments'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FilterChip(
+                  label: Text('All'),
+                  selected: _selectedRating == null,
+                  selectedColor: Colors.yellow,
+                  onSelected: (bool selected) {
+                    setState(() {
+                      _selectedRating = selected ? null : 0;
+                    });
+                  },
+                ),
+                for (int i = 1; i <= 5; i++)
+                  FilterChip(
+                    label: Text('$i Star'),
+                    selectedColor: Colors.yellow,
+                    selected: _selectedRating == i,
+                    onSelected: (bool selected) {
+                      setState(() {
+                        _selectedRating = selected ? i : null;
+                      });
+                    },
+                  ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: commentsList.length,
+              itemBuilder: (context, index) {
+                final comment = commentsList[index];
+                if (_selectedRating != null &&
+                    (_selectedRating != 0 &&
+                        comment['rating'] != _selectedRating)) {
+                  return SizedBox
+                      .shrink(); // Return empty SizedBox if rating doesn't match
+                }
+                return _buildCommentRow(comment);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildCommentRow(Map<String, dynamic> comment) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                radius: 20,
+                backgroundImage: NetworkImage(comment['avatar']),
+              ),
+              SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      comment['name'],
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 5),
+                    RatingBarIndicator(
+                      rating: comment['rating'],
+                      itemBuilder: (context, index) => Icon(
+                        Icons.star,
+                        color: Colors.amber,
+                      ),
+                      itemCount: 5,
+                      itemSize: 20,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 8),
+          Text(comment['comment']),
+          SizedBox(height: 8),
+          Wrap(
+            spacing: 8.0,
+            runSpacing: 4.0,
+            children: comment['chips'].map<Widget>((chipText) {
+              return Container(
+                padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                child: Text(
+                  chipText,
+                  style: TextStyle(color: Colors.grey),
+                ),
+              );
+            }).toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}

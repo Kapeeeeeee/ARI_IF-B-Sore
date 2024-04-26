074e2e4a399f86ef7d5519d2e2a1c30297f0bf02
Kapeeeeeee <165145804+Kapeeeeeee@users.noreply.github.com>
2024-04-26 13:16:26 +0700

    Menambah provider untuk membedakan alamat user

--- alamat.dart
+++ alamat.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kerkom/provider.dart';
+import 'package:tugas_kelompok_semester4/project/provider.dart';
 import 'package:provider/provider.dart';
 
 class AlamatPage extends StatefulWidget {
@@ -13,53 +13,75 @@ class _AlamatPageState extends State<AlamatPage> {
 
   @override
   Widget build(BuildContext context) {
-    final alamatProvider = Provider.of<AlamatProvider>(context);
+    final alamatProvider = Provider.of<AccountProvider>(context);
 
     return Scaffold(
       appBar: AppBar(
         title: Text("Pilih Alamat"),
       ),
       body: ListView.builder(
-          itemCount: alamatProvider.alamatList.length,
-          itemBuilder: (context, index) {
-            final alamat = alamatProvider.alamatList[index];
-            final isSelected = alamatProvider.selectedIndex == index;
-            return GestureDetector(
-              onTap: () {
-                setState(() {
-                  alamatProvider.setAlamat(alamat, index);
-                  selectedIndex = index;
-                });
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text("Alamat terpilih: ${alamat.nama}")),
-                );
-              },
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: isSelected ? Colors.orange : Colors.grey,
-                    width: 2,
-                  ),
-                ),
-                child: ListTile(
-                  title: Row(
-                    children: [
-                      Text(alamat.nama, style: TextStyle(
-                        fontSize: 14
-                      ),),
-                      Text(" | ${alamat.nohp}", style: TextStyle(
-                        fontSize: 12
-                      ),)
-                    ],
-                  ),
-                  subtitle: Text(alamat.detail, style: TextStyle(
-                    fontSize: 12
-                  ),),
-                  trailing: Text("Ongkir: Rp ${alamat.ongkir.toStringAsFixed(0)}"),
+        itemCount: alamatProvider.alamatList.length,
+        itemBuilder: (context, index) {
+          final alamat = alamatProvider.alamatList[index];
+          final isSelected = alamatProvider.selectedIndex == index;
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                alamatProvider.setAlamat(alamat, index);
+                selectedIndex = index;
+              });
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text("Alamat terpilih: ${alamat.nama}")),
+              );
+            },
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: isSelected ? Colors.orange : Colors.grey,
+                  width: 2,
                 ),
               ),
-            );
-          },
+              child: ListTile(
+                title: Row(
+                  children: [
+                    Flexible(
+                      child: Text(
+                        alamat.nama,
+                        style: TextStyle(
+                          overflow: TextOverflow.ellipsis,
+                          fontSize: 11,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                subtitle: Row(
+                  children: [
+                    Flexible(
+                      child: Text(
+                        alamat.detail,
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    Text(
+                      "${alamat.nohp}",
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+                trailing: Text(
+                  "Ongkir: Rp ${alamat.ongkir.toStringAsFixed(0)}",
+                  style: TextStyle(fontSize: 15),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }

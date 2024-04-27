2f4f9c744134115041d4af8cc931f831add978c6
221110286AlexAnderWijaya <165120273+221110286AlexAnderWijaya@users.noreply.github.com>
2024-04-28 02:18:50 +0700

    update

--- tampilan_produk.dart
+++ tampilan_produk.dart
@@ -19,126 +19,128 @@ class Makanan extends StatelessWidget {
       appBar: AppBar(
         title: Text('${g.judul}'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Hero(
-              tag: g.fileName,
-              child: Container(
-                height: 300, // Tentukan tinggi sesuai kebutuhan Anda
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(g.fileName),
-                    fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Hero(
+                tag: g.fileName,
+                child: Container(
+                  height: 300, 
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(g.fileName),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Text(
-                    g.judul,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                              builder: (context) => CommentPage()),
-                        );
-                      },
-                      icon: Icon(
-                        Icons.star,
-                        color: Colors.yellow,
-                      ),
-                    ),
-                    Text(
-                      g.rating.toString(),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Text(
+                      g.judul,
                       style: TextStyle(
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    SizedBox(width: 10),
-                    Text(
-                        NumberFormat.currency(
-                          locale: 'id_ID',
-                          symbol: '',
-                          decimalDigits: 0,
-                        ).format(int.parse(g.harga.toString())),
+                  ),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (context) => CommentPage()),
+                          );
+                        },
+                        icon: Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                        ),
+                      ),
+                      Text(
+                        g.rating.toString(),
                         style: TextStyle(
-                        fontSize: 20,
+                          fontSize: 20,
                           fontWeight: FontWeight.bold,
-                        )),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.blue[700],
-                    borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                          NumberFormat.currency(
+                            locale: 'id_ID',
+                            symbol: '',
+                            decimalDigits: 0,
+                          ).format(int.parse(g.harga.toString())),
+                          style: TextStyle(
+                          fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ],
                   ),
-                  child: IconButton(
-                    icon: Icon(Icons.remove, color: Colors.white),
-                    onPressed: () {
-                      Provider.of<AccountProvider>(context, listen: false)
-                          .kurangiItem(g.judul, itemCount);
-                    },
-                  ),
-                ),
-                SizedBox(width: 10),
-                Consumer<AccountProvider>(
-                  builder: (context, cartProvider, _) => Text(
-                    cartProvider.getItemCount(g.judul).toString(),
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue[700],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: IconButton(
+                      icon: Icon(Icons.remove, color: Colors.white),
+                      onPressed: () {
+                        Provider.of<AccountProvider>(context, listen: false)
+                            .kurangiItem(g.judul, itemCount);
+                      },
                     ),
                   ),
-                ),
-                SizedBox(width: 10),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.blue[700],
-                    borderRadius: BorderRadius.circular(10),
+                  SizedBox(width: 10),
+                  Consumer<AccountProvider>(
+                    builder: (context, cartProvider, _) => Text(
+                      cartProvider.getItemCount(g.judul).toString(),
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
-                  child: IconButton(
-                    icon: Icon(Icons.add, color: Colors.white),
-                    onPressed: () {
-                      Provider.of<AccountProvider>(context, listen: false)
-                          .tambahItem(
-                        g.fileName,
-                        g.judul,
-                        g.harga,
-                        g.itemCount,
-                        g.namatoko,
-                      );
-                    },
+                  SizedBox(width: 10),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue[700],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: IconButton(
+                      icon: Icon(Icons.add, color: Colors.white),
+                      onPressed: () {
+                        Provider.of<AccountProvider>(context, listen: false)
+                            .tambahItem(
+                          g.fileName,
+                          g.judul,
+                          g.harga,
+                          g.itemCount,
+                          g.namatoko,
+                        );
+                      },
+                    ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Text(
-              g.desc,
-              style: TextStyle(fontSize: 15),
-            ),
-          ],
+                ],
+              ),
+              SizedBox(height: 10),
+              Text(
+                g.desc,
+                style: TextStyle(fontSize: 15),
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -166,124 +168,126 @@ class Minuman extends StatelessWidget {
       appBar: AppBar(
         title: Text('${minum.judul}'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Hero(
-              tag: minum.fileName,
-              child: Container(
-                height: 300, // Tentukan tinggi sesuai kebutuhan Anda
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(minum.fileName),
-                    fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Hero(
+                tag: minum.fileName,
+                child: Container(
+                  height: 300, // Tentukan tinggi sesuai kebutuhan Anda
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(minum.fileName),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  minum.judul,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                              builder: (context) => CommentPage()),
-                        );
-                      },
-                      icon: Icon(
-                        Icons.star,
-                        color: Colors.yellow,
-                      ),
-                    ),
-                    Text(
-                      minum.rating.toString(),
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                        NumberFormat.currency(
-                          locale: 'id_ID',
-                          symbol: '',
-                          decimalDigits: 0,
-                        ).format(int.parse(minum.harga.toString())),
-                        style: TextStyle(
-                        fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        )),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.blue[700],
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: IconButton(
-                    icon: Icon(Icons.remove, color: Colors.white),
-                    onPressed: () {
-                      Provider.of<AccountProvider>(context, listen: false)
-                          .kurangiItem(minum.judul, itemCount);
-                    },
-                  ),
-                ),
-                SizedBox(width: 10),
-                Consumer<AccountProvider>(
-                  builder: (context, cartProvider, _) => Text(
-                    cartProvider.getItemCount(minum.judul).toString(),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    minum.judul,
                     style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-                SizedBox(width: 10),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.blue[700],
-                    borderRadius: BorderRadius.circular(10),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                                builder: (context) => CommentPage()),
+                          );
+                        },
+                        icon: Icon(
+                          Icons.star,
+                          color: Colors.yellow,
+                        ),
+                      ),
+                      Text(
+                        minum.rating.toString(),
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                          NumberFormat.currency(
+                            locale: 'id_ID',
+                            symbol: '',
+                            decimalDigits: 0,
+                          ).format(int.parse(minum.harga.toString())),
+                          style: TextStyle(
+                          fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ],
                   ),
-                  child: IconButton(
-                    icon: Icon(Icons.add, color: Colors.white),
-                    onPressed: () {
-                      Provider.of<AccountProvider>(context, listen: false)
-                          .tambahItem(
-                        minum.fileName,
-                        minum.judul,
-                        minum.harga,
-                        minum.itemCount,
-                        minum.namatoko,
-                      );
-                    },
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue[700],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: IconButton(
+                      icon: Icon(Icons.remove, color: Colors.white),
+                      onPressed: () {
+                        Provider.of<AccountProvider>(context, listen: false)
+                            .kurangiItem(minum.judul, itemCount);
+                      },
+                    ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 10),
-            Text(
-              minum.desc,
-              style: TextStyle(fontSize: 15),
-            ),
-          ],
+                  SizedBox(width: 10),
+                  Consumer<AccountProvider>(
+                    builder: (context, cartProvider, _) => Text(
+                      cartProvider.getItemCount(minum.judul).toString(),
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue[700],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: IconButton(
+                      icon: Icon(Icons.add, color: Colors.white),
+                      onPressed: () {
+                        Provider.of<AccountProvider>(context, listen: false)
+                            .tambahItem(
+                          minum.fileName,
+                          minum.judul,
+                          minum.harga,
+                          minum.itemCount,
+                          minum.namatoko,
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Text(
+                minum.desc,
+                style: TextStyle(fontSize: 15),
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(

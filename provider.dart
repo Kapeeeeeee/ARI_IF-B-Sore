cc2885d94c4a71765dfc60af7473a96f889ba63c
Kapeeeeeee <165145804+Kapeeeeeee@users.noreply.github.com>
2024-05-04 20:00:18 +0700

    Menambah Provider account dan CommentPage

--- provider.dart
+++ provider.dart
@@ -1,102 +1,118 @@
 import 'package:flutter/material.dart';
-
-class CartItem {
-  final String gambar;
-  final String itemName;
-  final int harga;
-  int itemCount;
-  final String namatoko;
-  bool isSelected;
+import 'package:kerkom/project/detail.dart';
 
 
-  CartItem({
-    required this.gambar,
-    required this.itemName,
-    required this.harga,
-    required this.itemCount,
-    required this.namatoko,
+class AccountProvider extends ChangeNotifier {
+  late List<Account> _accounts;
+  late Account _currentAccount;
 
-    this.isSelected = false, // Inisialisasi isSelected dengan false
-  });
-}
-
-class Alamat{
-  final String nama;
-  final String detail;
-  final double ongkir;
-  final String nohp;
-
-  Alamat(this.nama, this.detail, this.ongkir, this.nohp);
-  }
-
-
-class CartProvider extends ChangeNotifier {
-  List<CartItem> items = [];
-
-  // Fungsi untuk menambah item ke dalam keranjang berdasarkan nama item, harga, dan jumlah
-  void tambahItem(String gambar,String itemName, int harga, int itemCount,String namatoko) {
-    int existingIndex = items.indexWhere((item) => item.itemName == itemName);
-    if (existingIndex != -1) {
-      items[existingIndex].itemCount += itemCount;
+  AccountProvider({List<Account>? initialAccounts}) {
+    _accounts = initialAccounts ?? [];
+    if (_accounts.isNotEmpty) {
+      _currentAccount = _accounts[0];
     } else {
-      items.add(CartItem(gambar:gambar,itemName:itemName, harga:harga, itemCount:itemCount,namatoko: namatoko));
-    }
-    notifyListeners();
-  }
-  // Fungsi untuk mengurangi item dari keranjang berdasarkan nama item dan jumlah
-  void kurangiItem(String itemName, int itemCount) {
-    int existingIndex = items.indexWhere((item) => item.itemName == itemName);
-    if (existingIndex != -1) {
-      items[existingIndex].itemCount -= itemCount;
-      if (items[existingIndex].itemCount <= 0) {
-        items.removeAt(existingIndex);
-      }
-      notifyListeners();
+      _currentAccount = Account(
+        namauser: 'Default Account',
+        foto: "tugas_kelompok_semester4/assets/logo.png",
+        nohp: "-",
+        alamat: [Alamat("-", "-", 0, "-")],
+        deskripsi: "akun demo",
+        uang: 0,
+        cart: [],
+      );
     }
   }
-  // Fungsi untuk mendapatkan jumlah item dari suatu jenis
-  int getItemCount(String itemName) {
-    int existingIndex = items.indexWhere((item) => item.itemName == itemName);
-    return existingIndex != -1 ? items[existingIndex].itemCount : 0;
-  }
 
-  // Fungsi untuk mendapatkan harga dari suatu jenis item
-  int getPrice(String itemName) {
-    int existingIndex = items.indexWhere((item) => item.itemName == itemName);
-    return existingIndex != -1 ? items[existingIndex].harga : 0;
-  }
+  List<Account> get accounts => _accounts;
+  Account get currentAccount => _currentAccount;
 
-void toggleItemSelection(CartItem item) {
-    item.isSelected = !item.isSelected;
-    notifyListeners();
-  }
-void hapusItem(CartItem item) {
-    items.remove(item);
-    notifyListeners();
-  }
-  
-}
-
-class AlamatProvider extends ChangeNotifier{
   int? _selectedIndex;
   Alamat? _alamatSelected;
-
+  
   Alamat? get alamatSelected => _alamatSelected;
   int? get selectedIndex => _selectedIndex;
 
-  get alamatList => [
-  Alamat("Rumah Alex", "Jl. Retribution No. 1, Kota Indomaret", 15000.0, "082355694151"),
-  Alamat("Kantor Frederick", "Jl. Tembak No. 2, Kota Gamon", 20000.0, "081225648792"),
-  Alamat("Gudang Richardo", "Jl. Polisi No. 3, Kota Tidur", 25000.0, "085845654864"),
-  Alamat("Gudang Kenzie", "Jl. Dark System No. 5, Kota Apaaja", 15000.0, "085235478468"),
-  ];
+  get alamatList => _currentAccount.alamat;
 
   void setAlamat(Alamat alamat, int index) {
     _alamatSelected = alamat;
     _selectedIndex = index;
+    notifyListeners();}
+
+
+  
+
+  void switchAccount(Account newAccount) {
+    _currentAccount = newAccount;
+    notifyListeners();
+  }
+
+
+  void tambahItem(String gambar, String itemName, int harga, int itemCount, String namatoko) {
+    int existingIndex = currentAccount.cart.indexWhere((item) => item.itemName == itemName);
+    if (existingIndex != -1) {
+      _currentAccount.cart[existingIndex].itemCount += itemCount;
+    } else {
+      _currentAccount.cart.add(CartItem(gambar:gambar,itemName:itemName, harga:harga, itemCount:itemCount,namatoko: namatoko));
+    }
+    
+    notifyListeners();
+  }
+
+  // Fungsi untuk mengurangi jumlah item dalam keranjang belanja
+  void kurangiItem(String itemName, int itemCount) {
+    int existingIndex = _currentAccount.cart.indexWhere((item) => item.itemName == itemName);
+    if (existingIndex != -1) {
+      _currentAccount.cart[existingIndex].itemCount -= itemCount;
+      if (_currentAccount.cart[existingIndex].itemCount <= 0) {
+        _currentAccount.cart.removeAt(existingIndex);
+      }
+      notifyListeners();
+    }
+  }
+
+  // Fungsi untuk mendapatkan jumlah item dalam keranjang belanja
+  int getItemCount(String itemName) {
+    int existingIndex = _currentAccount.cart.indexWhere((item) => item.itemName == itemName);
+    return existingIndex != -1 ? _currentAccount.cart[existingIndex].itemCount : 0;
+  }
+
+  // Fungsi untuk mendapatkan harga dari suatu jenis item dalam keranjang belanja
+  int getPrice(String itemName) {
+    int existingIndex = _currentAccount.cart.indexWhere((item) => item.itemName == itemName);
+    return existingIndex != -1 ? _currentAccount.cart[existingIndex].harga : 0;
+  }
+
+  // Fungsi untuk memilih atau tidak memilih item dalam keranjang belanja
+  void toggleItemSelection(CartItem item) {
+    item.isSelected = !item.isSelected;
+    notifyListeners();
+  }
+
+  // Fungsi untuk menghapus item dari keranjang belanja
+  void hapusItem(CartItem item) {
+    _currentAccount.cart.remove(item);
     notifyListeners();
   }
 }
+
+
+
+// Kelas ThemeModeProvider untuk mengelola tema aplikasi
+class ThemeModeProvider with ChangeNotifier {
+  bool _themeMode = false;
+
+  bool get themeMode => _themeMode;
+
+  // Metode untuk mengubah status tema
+  void toggleTheme(bool val) {
+    _themeMode = val;
+    notifyListeners();
+  }
+}
+
+
+
 
 class PromoProvider extends ChangeNotifier {
   int? _promoPercentage;
@@ -124,11 +140,27 @@ class PromoProvider extends ChangeNotifier {
   }
 }
 
+
+
+
 class PriorityProvider extends ChangeNotifier {
   bool isPriority = false;
 
   void setPriority(bool val){
     isPriority = val;
+    notifyListeners();
+  }
+}
+
+
+
+class CommentData extends ChangeNotifier {
+  List<Map<String, dynamic>> _commentsList = [];
+
+  List<Map<String, dynamic>> get commentsList => _commentsList;
+
+  void addComment(Map<String, dynamic> commentData) {
+    _commentsList.insert(0, commentData);
     notifyListeners();
   }
 }

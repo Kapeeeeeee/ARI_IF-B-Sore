90f311c5e9e1ca0962493a60f61db1f8742aa6a4
221111834-RichardoWijaya <166472136+221111834-RichardoWijaya@users.noreply.github.com>
2024-05-07 17:29:04 +0700

    Update accountSafety.dart

--- accountSafety.dart
+++ accountSafety.dart
@@ -1,30 +1,43 @@
 import 'package:flutter/material.dart';
 
-class LanguageChangePage extends StatelessWidget {
+class AccountSafetyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Change Language'),
+        title: Text('Account Safety'),
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            LanguageOption(
-              language: 'English',
+            Text(
+              'Account Safety',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            SafetyOption(
+              title: '2-Step Authentication',
+              description:
+                  'Enable additional security measures for your account.',
               onPressed: () {
                 // logik
               },
             ),
             SizedBox(height: 16.0),
-            LanguageOption(
-              language: 'Indonesian',
+            SafetyOption(
+              title: 'Fingerprint Authentication',
+              description:
+                  'Use fingerprint authentication to access your account.',
               onPressed: () {
                 // logik
               },
             ),
+            SizedBox(height: 16.0),
           ],
         ),
       ),
@@ -32,20 +45,26 @@ class LanguageChangePage extends StatelessWidget {
   }
 }
 
-class LanguageOption extends StatelessWidget {
-  final String language;
+class SafetyOption extends StatelessWidget {
+  final String title;
+  final String description;
   final VoidCallback onPressed;
 
-  const LanguageOption({
-    required this.language,
+  const SafetyOption({
+    required this.title,
+    required this.description,
     required this.onPressed,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Text(language),
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(description),
+      trailing: IconButton(
+        icon: Icon(Icons.arrow_forward),
+        onPressed: onPressed,
+      ),
     );
   }
 }
